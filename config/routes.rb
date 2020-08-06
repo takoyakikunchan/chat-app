68644b924822a6fa1a6e3fe003375de61cd01d43
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-06 16:21:08 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'messages/index'
   root "messages#index"
   resources :users, only: [:edit, :update]
-
+  resources :rooms, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
