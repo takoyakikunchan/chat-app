7d63983c977df6525c1b9e5972cb67fc13e76e4a
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-08-07 09:27:12 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -15,7 +16,7 @@ class MessagesController < ApplicationController
       
     end
 
-  end
+  
    private
   def message_params
      params.require(:message).permit(:content).merge(user_id: current_user.id)
